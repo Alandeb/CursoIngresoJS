58ce2ebe56015c79cc618f4b51020b0d663d3035
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-03 16:47:36 -0300

    Hecho en casa
    se descubrio errores se busco en internet por una solucion gracias ro :3

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,16 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var edad;
+	var nombre;	
+	nombre=document.getElementById('elNombre').value;	
+	edad=document.getElementById('laEdad').value;
+	alert(" Usted se llama "+nombre+" y tiene "+edad+" años. ");
 }
-
+/* me salta un problema en el navegador averiguar despues 
+*/
+/*
+	problema resuelte pasa que puse nombre y una coma eso hacia que me 
+	la variable y no funcionara , aparte en JS  si quiere agregar una variablwe
+	tengo que poner mas al inicio y al final para que funcione 
+*/ 	
