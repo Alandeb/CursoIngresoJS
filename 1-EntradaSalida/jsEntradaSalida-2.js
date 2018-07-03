7cc3c72008739b5c62c267ac4e703507459a24b5
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-02 22:04:58 -0300

    Clase n1
    se hizo hasta ejercicio 4 se vio alert prompt var document.getelementbyid hasta ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;		
+	//nombre = "Alan";
+	//alert(nombre);
+	nombre = prompt("Ingrese su nombre", "natalia natalia");
+	alert("Su nombre es: "+nombre);			
+	//var person = prompt("Please enter your name", "Harry Potter"); // sacado de 23school
 }
-
