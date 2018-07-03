b155c5a887ded0f45770a2ff1fbb96230d3b67ed
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-02 21:55:33 -0300

    Dia uno
    Primer dia de programacion

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombre;		
+	//nombre = "Alan";
+	//alert(nombre);
+	nombre = prompt("Ingrese su nombre", "natalia natalia");
+	alert("Su nombre es: "+nombre);			
+	//var person = prompt("Please enter your name", "Harry Potter"); // sacado de 23school
+}
