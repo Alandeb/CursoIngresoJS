2dad51f7dacaeb4d72d7e896555d8daa7820072a
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-04 17:35:09 -0300

    Hecho en casa 4/7
    este ejercicio es buscando numeros en prompt y lo da por alert  se usa parseint

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
-
+	var a;	
+	var b;
+	var r;	
+ 	a = prompt("Ingrese ancho del rectangulo ","0");	
+ 	a = parseInt(a); 
+ 	b = prompt("Ingrese largo del rectangulo ","0");
+ 	b = parseInt(b);// el parseInt sirve para hace que la variable pase a numero si no es solo una letra 
+	r = 2*a + 2*b;
+ 	alert("Su perimetro es: "+r);
 }
