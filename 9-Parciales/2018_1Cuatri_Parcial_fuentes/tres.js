5644d16ceb67e9a812a9f9715968251fd17e79f4
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-05 17:24:54 -0300

    Hecho en casa 5/7
    si hizo el descuento

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
-
+	var p;	
+	var	d;
+	var	r;
+	var resta;
+	p=prompt("Ingrese precio ","0");
+	p=parseInt(p);	
+	d=prompt("Ingrese descuento ","0");
+	d=parseInt(d);
+	resta=100-d;// se agrego otra variable por que no se podia hacer con %
+	r= resta*p/100;
+	document.getElementById('elPrecioFinal').value=r;//si coloco algo entre comillas despues del var no funciona el programa   
 }
+
