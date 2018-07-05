40c9b901448b6943ba09010bbd619859a288ad7a
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-05 17:26:16 -0300

    Hecho en casa 5/7
    igual que le de rectángulo pero con un triangulo se uso una variable mas

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
-
+	var a;	
+	var b;
+	var p;	
+	var sup;
+ 	a = prompt("Ingrese la base del triangulo equilatero ","0");	
+ 	a = parseInt(a); 
+ 	b = prompt("Ingrese el largo del triangulo equilatero ","0");
+ 	b = parseInt(b);// el parseInt sirve para hace que la variable pase a numero si no es solo una letra 
+	p = 3*a;//poner la p en mayus hizo que la variable cambien tener en cuenta 
+	sup = (a*b)/2;
+ 	alert("Su perimetro es: "+p+" y su superficie es: "+sup);
 }
