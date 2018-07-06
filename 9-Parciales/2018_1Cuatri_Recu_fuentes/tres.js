d8c645355ff9afac7ae1482256286bef3d575186
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-05 22:00:59 -0300

    Clase n2 5/7
    el uno se hizo en casa pero se cambio las variables

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,21 @@
 function mostrar()
-{
+{	
+	var	precio;
+	var descuento;
+	var	descdinero;
+	var	precdesc;
+	var	preciofinal;
+	var iva;
 
+	precio=prompt("ingrese precio ","0");
+	precio=parseInt(precio);
+	descuento=prompt("ingrese descuento ","0");
+	descuento=parseInt(descuento);
+	precdesc=(100-descuento)*precio/100;
+	descdinero=precio-precdesc;
+	iva=(121*precio)/100;
+	alert("se le descontara "+descdinero+" ,el precio con descuento "+precdesc+" y el precio con iva "+iva); 
+	preciofinal=(121*precdesc)/100;
+	document.getElementById('elPrecioFinal').value=preciofinal;
+	
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,15 +1,15 @@
 
 function mostrar()
 {
-	var a;	
-	var b;
-	var p;	
-	var sup;
- 	a = prompt("Ingrese la base del triangulo equilatero ","0");	
- 	a = parseInt(a); 
- 	b = prompt("Ingrese el largo del triangulo equilatero ","0");
- 	b = parseInt(b);// el parseInt sirve para hace que la variable pase a numero si no es solo una letra 
-	p = 3*a;//poner la p en mayus hizo que la variable cambien tener en cuenta 
-	sup = (a*b)/2;
- 	alert("Su perimetro es: "+p+" y su superficie es: "+sup);
+	var base;	
+	var altura;
+	var perimetro;	
+	var superficie;
+ 	base = prompt("Ingrese la base del triangulo equilatero ","0");	
+ 	base = parseInt(base); 
+ 	altura = prompt("Ingrese el largo del triangulo equilatero ","0");
+ 	altura = parseInt(altura);// el parseInt sirve para hace que la variable pase a numero si no es solo una letra 
+	perimetro = 3*base;//poner la p en mayus hizo que la variable cambien tener en cuenta 
+	superficie = (base*altura)/2;
+ 	alert("Su perimetro es: "+perimetro+" y su superficie es: "+superficie);
 }
