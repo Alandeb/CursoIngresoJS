e7acfeb943f6ddb0910923d08c246c2539030356
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-26 17:20:29 -0300

    Listo
    if y switch

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+	var planeta;
+	planeta=prompt("Ingrese un planeta ");
+	switch(planeta){
+		case "mercurio":
+		case "venus":
+			alert("acá hace más calor");
+			break;
+		case "tierra":
+			alert("aca vivimos");
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":// si no coloco las comillas no me toma la variable de los casos
+		case "urano":
+		case "neptuno":
+			alert("acá hace más frio");
+			break;
+		default :
+			alert("no es un planeta valido volver a intentarlo ");			
+	}
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
-
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno=prompt("Coleque numero uno");
+	numerouno=parseInt(numerouno);
+	numerodos=prompt("Coleque numero dos");
+	numerodos=parseInt(numerodos);
+	if (numerouno==numerodos) {
+		alert("Su numero uno es "+numerouno+" y su numero dos es "+numerodos+" son iguales");
+	 }else{
+	 	if (numerouno>numerodos) {
+	 		resultado=numerouno-numerodos;
+	 		alert("El resultado es "+resultado);
+	 	}else{
+	 		resultado=numerouno+numerodos
+	 		alert("El resultado es "+resultado);
+	 		}
+	 	}
+	if (resultado>10 && (numerouno<numerodos)) {
+		alert("la suma es "+resultado+" y supero el 10");
+	 			
+	}else{}		 
 }
