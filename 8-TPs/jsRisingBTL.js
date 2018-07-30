f1243497c44c6476f16696e4857e2a01011f7e27
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-07-30 17:45:22 -0300

    Falta terminar los id

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,81 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+ 	var edad;
+ 	var	sexo;
+ 	var	estadocivil;
+ 	var	sueldo;
+ 	var	legajo;
+ 	var	nacionalidad;
+ 	edad=prompt("coloque una edad entre 18 y 90 inclusive");
+ 	while(edad<18 || edad>90)
+ 	{
+ 		edad=prompt("coloque una edad entre 18 y 90 inclusive");
+
+ 	}
+ 	sexo=prompt("coloque su sexo 'f' para femenino o 'm' para masculino","use minusculas");
+ 	while(sexo!="f" &&	sexo!="m")
+ 	{
+ 		sexo=prompt("coloque su sexo 'f' para femenino o 'm' para masculino","use minusculas");
+ 	}
+ 	if (sexo=="f") 
+ 	{
+ 		sexo=="femenino";
+ 	}else
+ 		{
+ 			sexo="masculino";
+ 		}
  
+	estadocivil=prompt("coloque su estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+	while(estadocivil<1||estadocivil>4)
+	{
+		estadocivil=prompt("coloque su estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+		
+	}
+	if (estadocivil==1) 
+		{
+			estadocivil= "soltero";
+	
+		}else
+			if (estadocivil==2) 
+			{
+				estadocivil="casados";
+				
+			}else
+				if (estadocivil==3)
+				{
+					estadocivil="divorciado";
+				
+				}else
+					if (estadocivil==4) 
+					{
+						estadocivil="viudo";
+					}
+	sueldo=prompt("ingrese el sueldo bruto debera ser mayor a 8000");				
+	while(sueldo<8000)
+	{
+		sueldo=prompt("ingrese el sueldo bruto debera ser mayor a 8000");
+	}			
+	legajo=prompt("ingrese su numero de legajo");
+	while(legajo<1000 || legajo>9999)
+	{
+		legajo=prompt("ingrese su numero de legajo");
+	}
+	nacionalidad=prompt("ingrese “a” para argentinos, “e” para extranjeros, “n” para nacionalizados en minusculas" );
+	while(nacionalidad!=a && nacionalidad!=e && nacionalidad!=n)
+	{
+		nacionalidad=prompt("ingrese “a” para argentinos, “e” para extranjeros, “n” para nacionalizados en minusculas" );
+	}
+	if (nacionalidad=="a") 
+	{
+		nacionalidad="argentino/a";
+	}else
+		if (nacionalidad=="e") 
+		{
+			nacionalidad="extranjero/a"
+		}else
+			{
+				nacionalidad="nacionalizado/a";	
+			}
+
 }
