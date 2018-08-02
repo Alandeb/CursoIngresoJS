5f0fdfd1c0db9ff7fcfdfd9c1c3585226a7531e2
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-08-02 19:31:55 -0300

    Terminado/ no parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -20,7 +20,12 @@ function mostrar()
 	var edadhombres=0;
 	var edadtotal=0;
 	var contador=0;
-	var cantidadpersonas;
+	var	sexomayor;
+	var	nombremayor;
+	var nombremenor;	 	
+	var promediototal;
+	var	edadmaximafem;
+	var nombremayorfem;	
 	while(contador<4)
 	{
 		contador++;
@@ -42,7 +47,6 @@ function mostrar()
 		{
 			cantidadmujeres++;
 			edadmujeres=(edadmujeres+edad)/cantidadmujeres;
-
 		}else
 		{
 			cantidadhombres++;
@@ -59,22 +63,29 @@ function mostrar()
 		{
 			edadminima=edad;
 			edadmaxima=edad;
+			nombremayor=nombre;
+			sexomayor=sexo;
 
 		}else
 			if (edad>edadmaxima) 
 			{
 				edadmaxima=edad;
+				nombremayor=nombre;
+				sexomayor=sexo;
 			}
 			if (edad<edadminima) 
 			{
 				edadminima=edad;
+				nombremenor=nombre;
 			}
-		cantidadpersonas=cantidadmujeres+cantidadhombres;	
-		edadtotal=(edadtotal+edad)/cantidadpersonas;	
-
+			
+		edadtotal=edadtotal+edad;	
+		promediototal=edadtotal/contador;	
 	}
 	document.write("<BR>Cantidad de hombres es: "+cantidadhombres+"<BR>Cantidad de mujeres es: "+cantidadmujeres);
 	document.write("<BR>Cantidad de mayores es: "+cantidadmayores+"<BR>Cantidad de menores es: "+cantidadmenores);
 	document.write("<BR>Edad de la persona mas grande: "+edadmaxima+"<BR>Edad de la persona mas joven: "+edadminima);
-	document.write("<BR>Promedio edad mujeres: "+edadmujeres+"<BR>Promedio edad hombres: "+edadhombres+"<BR>Promedio de edad total: "+edadtotal);
+	document.write("<BR>Promedio edad mujeres: "+edadmujeres+"<BR>Promedio edad hombres: "+edadhombres+"<BR>Promedio de edad total: "+promediototal);
+	document.write("<BR>Nombre del mas viejo: "+nombremayor+"<BR>Nombre mas viejo femenino: "+nombremayorfem);
+	document.write("<BR>Nombre del mas joven: "+nombremenor+"<BR>sexo del mas viejo: "+sexomayor);	
 }
