6c63e81a17e2df942979182de1be3e3e8b3aeb12
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-08-06 22:09:17 -0300

    Terminar

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+	var nombre;
+	var temperatura;
+	var peso;
+	var contadoranimales=0;
+	var contadorpesospar=0;
+
+
+	while(true)
+	{
+		contadoranimales++1;
+		nombre=prompt("ingrese el nombre del animal (y coloque 'no' si desea terminar) ");
+		if(nombre==no)
+		{
+			break;
+		}
+		temperatura=prompt("ingrese la temperatura de habitat (entre -30 y 30 grados)");
+		temperatura=parseInt(temperatura);
+		while(isNaN(temperatura)||temperatura<-30||temperatura>30)
+		{
+			temperatura=prompt("ingrese la temperatura de habitat (entre -30 y 30 grados)");
+			temperatura=parseInt(temperatura);
+		}
+		peso=prompt("ingrese un peso entre 1 y 1000 inclusive");
+		peso=parseInt(peso);
+		while(isNaN(peso)||peso<1||peso>1000)
+		{
+			
+			peso=prompt("ingrese un peso entre 1 y 1000 inclusive");
+			peso=parseInt(peso);
+		}
+		if (peso%2==0)
+		 {
+		 	contadorpesospar++;
+		 }
+		if (contadoranimales==1)
+		 {
+		 	
+		 } 
+
+
+
+
+
+	}
+
 
 }
